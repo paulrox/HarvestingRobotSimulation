4b331afd4b022d927d7df06af0b60c604f8a989c
Paolo Sassi <paolo.sassi91@gmail.com>
2017-06-22 16:21:03 +0200

    created robot arm

---
+++ Arm_config.m
@@ -0,0 +1,37 @@
+%% Initialize the toolbox
+
+%close all;
+%clearvars;
+%startup_rvc;
+
+%% Robot arm configuration
+
+% Arm links
+
+R1 = Link('d', -0.9, 'a', 0.188, 'alpha', -pi/2);
+R2 = Link('d', 0, 'a', 0.95, 'alpha', 0);
+R3 = Link('d', 0, 'a', 0.225, 'alpha', -pi/2);
+R4 = Link('d', -1.705, 'a', 0, 'alpha', pi/2);
+R5 = Link('d', 0, 'a', 0, 'alpha', -pi/2);
+R6 = Link('d', -0.2, 'a', 0, 'alpha', -pi/2);
+
+arm = SerialLink([R1 R2 R3 R4 R5 R6], 'name', 'arm');
+
+qn = [pi 0.7854 3.1416 0 0.7854 0];
+
+% Platform links
+
+P1 = Link('theta', 0, 'a', 0.5, 'alpha', -pi/2);
+P2 = Link('theta',-pi/2, 'a', 1.5, 'alpha', 0);
+
+platform = SerialLink([P1 P2], 'name', 'platform');
+platform.base = trotx(pi/2);
+
+robot = SerialLink([platform arm], 'name', 'robot');
+
+%figure;
+
+qn = [0 0 qn];
+
+robot.plotopt = {'workspace' [-3 3 -3 3 -4 2] 'scale' 0.5, 'jvec'};
+robot.plot(qn);
