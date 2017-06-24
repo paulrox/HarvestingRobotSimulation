1de9dcd76068b8eb78190c6129836c6956a89365
Paolo Sassi <paolo.sassi91@gmail.com>
2017-06-24 11:09:42 +0200

    fix trials

--- Arm_config.m
+++ Arm_config.m
@@ -31,17 +31,20 @@ h_robot = SerialLink([platform arm], 'name', 'h_robot');
 
 %figure;
 
-qn = [0 0 qn];
+qn = [qn];
 
 %% Inverse Differential Kinematics
 
-dt = 1;
-test_T0 = h_robot.fkine(qn);
-test_T1 = trotx(-pi/2) * transl(3, 0, 0);
-test_traj = ctraj(test_T0, test_T1, 20);
+dt = 0.01;
+n_step = 200;
+% test_T0 = h_robot.fkine(qn);
+test_T0 = p560.fkine(qn);
+% test_T1 = transl(1, -1, -1) * r2t(t2r(test_T0));
+test_T1 = test_T0 * transl(1, 0, 0);
+test_traj = ctraj(test_T0, test_T1, n_step);
 
-test_v = zeros(6,1,20);
-for i = 1:19
+test_v = zeros(6,1,n_step);
+for i = 1 : n_step-1
     test_v(:,:,i+1) = (tr2delta(test_traj(:,:,i), test_traj(:,:,i+1)))/dt;
 end;
 
@@ -50,22 +53,27 @@ q = struct;
 
 % null space optimization
 
-qd.no_opt = zeros(1,8,20);
-q.no_opt = zeros(21,8);
-q.no_opt(1,:) = qn;
+qd.no_opt = zeros(1,6,n_step);
+q.no_opt = zeros(n_step+1,6);
+q.no_opt(1,:,:) = qn;
 
-for i = 1:20
-    J = jacob0(h_robot, qd.no_opt(:,:,i));
+J = p560.jacob0(qn);
+for i = 1:n_step
+    % J = h_robot.jacob0(qd.no_opt(:,:,i));
     % q0 = null_opt(h_robot, 'manip', q.no_opt(i,:));
-    q0 = [0 0 10 10 10 10 10 10]; 
-    qd.no_opt(:,:,i) = pinv(J)*test_v(:,:,i) + (eye(8) - pinv(J)*J)*q0';
+    q0 = [0 0 2 2 2 2 2 2];
+    % qns = (eye(8) - pinv(J)*J)*q0';
+    % qd.no_opt(:,:,i) = pinv(J)*test_v(:,:,i) + qns;
+    qd.no_opt(:,:,i) = inv(J)*test_v(:,:,i);
+    J = p560.jacob0(qd.no_opt(:,:,i));
     q.no_opt(i+1,:) = q.no_opt(i,:) + qd.no_opt(:,:,i)*dt; 
 end;
 
 %% Plot the robot
 
-h_robot.plotopt = {'workspace' [-3 3 -3 3 -4 2] 'scale' 0.5, 'jvec'};
-h_robot.plot(q.no_opt);
+% h_robot.plotopt = {'workspace' [-3 3 -3 3 -4 2] 'scale' 0.5, 'jvec'};
+% h_robot.plot(q.no_opt);
+p560.plot(q.no_opt);
 
 %% Task Objects
 
