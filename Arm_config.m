b63371952174dd978bf3cd122f6b71b2ef8ca7cf
Paolo Sassi <paolo.sassi91@gmail.com>
2017-06-24 12:09:24 +0200

    fixed trajectory, null space bug

--- Arm_config.m
+++ Arm_config.m
@@ -1,5 +1,4 @@
 %% Initialize the toolbox
-clear
 %close all;
 %clearvars;
 %startup_rvc;
@@ -27,53 +26,45 @@ P2 = Link('theta',-pi/2, 'a', 1.5, 'alpha', 0);
 platform = SerialLink([P1 P2], 'name', 'platform');
 platform.base = trotx(pi/2);
 
-h_robot = SerialLink([platform arm], 'name', 'h_robot');
+robot = SerialLink([platform arm], 'name', 'h_robot');
 
-%figure;
-
-qn = [qn];
+qn = [0 0 qn];
 
 %% Inverse Differential Kinematics
 
-dt = 0.01;
-n_step = 200;
-% test_T0 = h_robot.fkine(qn);
-test_T0 = p560.fkine(qn);
-% test_T1 = transl(1, -1, -1) * r2t(t2r(test_T0));
-test_T1 = test_T0 * transl(1, 0, 0);
-test_traj = ctraj(test_T0, test_T1, n_step);
-
-test_v = zeros(6,1,n_step);
-for i = 1 : n_step-1
-    test_v(:,:,i+1) = (tr2delta(test_traj(:,:,i), test_traj(:,:,i+1)))/dt;
-end;
-
-qd = struct;
-q = struct;
-
-% null space optimization
-
-qd.no_opt = zeros(1,6,n_step);
-q.no_opt = zeros(n_step+1,6);
-q.no_opt(1,:,:) = qn;
-
-J = p560.jacob0(qn);
-for i = 1:n_step
-    % J = h_robot.jacob0(qd.no_opt(:,:,i));
-    % q0 = null_opt(h_robot, 'manip', q.no_opt(i,:));
-    q0 = [0 0 2 2 2 2 2 2];
-    % qns = (eye(8) - pinv(J)*J)*q0';
-    % qd.no_opt(:,:,i) = pinv(J)*test_v(:,:,i) + qns;
-    qd.no_opt(:,:,i) = inv(J)*test_v(:,:,i);
-    J = p560.jacob0(qd.no_opt(:,:,i));
-    q.no_opt(i+1,:) = q.no_opt(i,:) + qd.no_opt(:,:,i)*dt; 
-end;
-
-%% Plot the robot
-
-% h_robot.plotopt = {'workspace' [-3 3 -3 3 -4 2] 'scale' 0.5, 'jvec'};
-% h_robot.plot(q.no_opt);
-p560.plot(q.no_opt);
+% dt = 0.01;
+% n_step = 200;
+% % test_T0 = h_robot.fkine(qn);
+% test_T0 = p560.fkine(qn);
+% % test_T1 = transl(1, -1, -1) * r2t(t2r(test_T0));
+% test_T1 = test_T0 * transl(1, 0, 0);
+% test_traj = ctraj(test_T0, test_T1, n_step);
+% 
+% test_v = zeros(6,1,n_step);
+% for i = 1 : n_step-1
+%     test_v(:,:,i+1) = (tr2delta(test_traj(:,:,i), test_traj(:,:,i+1)))/dt;
+% end;
+% 
+% qd = struct;
+% q = struct;
+% 
+% % null space optimization
+% 
+% qd.no_opt = zeros(1,6,n_step);
+% q.no_opt = zeros(n_step+1,6);
+% q.no_opt(1,:,:) = qn;
+% 
+% J = p560.jacob0(qn);
+% for i = 1:n_step
+%     % J = h_robot.jacob0(qd.no_opt(:,:,i));
+%     % q0 = null_opt(h_robot, 'manip', q.no_opt(i,:));
+%     q0 = [0 0 2 2 2 2 2 2];
+%     % qns = (eye(8) - pinv(J)*J)*q0';
+%     % qd.no_opt(:,:,i) = pinv(J)*test_v(:,:,i) + qns;
+%     qd.no_opt(:,:,i) = inv(J)*test_v(:,:,i);
+%     J = p560.jacob0(qd.no_opt(:,:,i));
+%     q.no_opt(i+1,:) = q.no_opt(i,:) + qd.no_opt(:,:,i)*dt; 
+% end;
 
 %% Task Objects
 
@@ -83,31 +74,55 @@ Ytree = [-3 -3 3 3];
 Ztree = [-4 2 2 -4];
 %fruit_tree = [3 -3 -4; 3 -3 2; -3 -3 2; -3 -3 -4];
 %fruit_tree = [-3 -3 -4; -3 -3 2; -3 3 2; -3 3 -4];
-fruit_tree = [Xtree; Ytree; Ztree]
+fruit_tree = [Xtree; Ytree; Ztree];
 
 % Fruit
-C_fruit = [-3; 0; 0.5]
+C_fruit = [-3; 0; 0.5];
 R_fruit = 0.15;
 
 plot_poly(fruit_tree, 'fill', 'g');
 plot_sphere(C_fruit, R_fruit, 'color', 'r');
 
 %% Building a trajectory
-N = 100;
-dt = 2;
+N = 200;
+dt = 1;
 T0 = robot.fkine(qn);
 T1 = transl(1, 0, 2) * robot.base;
 TC = ctraj(T0, T1, N);
-ve = [];
+ve = zeros(N-1, 6);
 
-q(1,:) = qn;
+no_opt = struct;
+manip = struct;
+
+no_opt.q = zeros(N, 8);
+no_opt.q(1,:) = qn;
+no_opt.qdot = zeros(N-1, 8);
+
+manip.q = zeros(N, 8);
+manip.q(1,:) = qn;
+manip.qdot = zeros(N-1, 8);
+manip.q0 = zeros(1,8);
+manip.qns = zeros(1,8);
+
 for i = 1: (N)
     if (i ~= N)
         ve(i, :) = tr2delta(TC(:, :, i), TC(:, :, i+1)) / dt;
-        J = jacob0(robot, q(i,:));
-        qdot(i, :) = pinv(J) * ve(i, :)'
-        q(i+1, :) = q(i, :) + (qdot(i, :)*dt);
+        
+        % No optimization jacobian and joint positions
+        no_opt.J = jacob0(robot, no_opt.q(i,:));
+        no_opt.qdot(i,:) = pinv(no_opt.J) * ve(i,:)';
+        no_opt.q(i+1,:) = no_opt.q(i,:) + (no_opt.qdot(i,:)*dt);
+        
+        % Null space optimization - Manipulability
+        manip.J = jacob0(robot, manip.q(i,:));
+        manip.q0 = [0 0 1 1 1 1 1 1];
+        manip.qns = (eye(8) - pinv(manip.J) * manip.J) * manip.q0';
+        manip.qdot(i,:) = pinv(manip.J) * ve(i,:)' + manip.qns;
+        manip.q(i+1,:) = manip.q(i,:) + (manip.qdot(i,:)*dt);
     end   
 end
 
+%% Plot the robot
 
+robot.plotopt = {'workspace' [-3 3 -3 3 -4 2] 'scale' 0.5, 'jvec'};
+robot.plot(no_opt.q);
