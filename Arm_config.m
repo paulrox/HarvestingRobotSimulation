a64d0e6845c766c13c990442d1cd5ecb2917a6ae
Paolo Sassi <paolo.sassi91@gmail.com>
2017-06-26 15:03:23 +0200

    fixed optimization

--- Arm_config.m
+++ Arm_config.m
@@ -28,43 +28,13 @@ platform.base = trotx(pi/2);
 
 robot = SerialLink([platform arm], 'name', 'h_robot');
 
+% Joint limits
+robot.qlim = [-0.1 0.1;-0.1 0.1; -deg2rad(135) deg2rad(135); ...
+    -deg2rad(45) deg2rad(170); deg2rad(140) deg2rad(220); ...
+    -deg2rad(170) deg2rad(170); deg2rad(0) deg2rad(180); ...
+    -deg2rad(170) deg2rad(170)];
+
 qn = [0 0 qn];
-
-%% Inverse Differential Kinematics
-
-% dt = 0.01;
-% n_step = 200;
-% % test_T0 = h_robot.fkine(qn);
-% test_T0 = p560.fkine(qn);
-% % test_T1 = transl(1, -1, -1) * r2t(t2r(test_T0));
-% test_T1 = test_T0 * transl(1, 0, 0);
-% test_traj = ctraj(test_T0, test_T1, n_step);
-% 
-% test_v = zeros(6,1,n_step);
-% for i = 1 : n_step-1
-%     test_v(:,:,i+1) = (tr2delta(test_traj(:,:,i), test_traj(:,:,i+1)))/dt;
-% end;
-% 
-% qd = struct;
-% q = struct;
-% 
-% % null space optimization
-% 
-% qd.no_opt = zeros(1,6,n_step);
-% q.no_opt = zeros(n_step+1,6);
-% q.no_opt(1,:,:) = qn;
-% 
-% J = p560.jacob0(qn);
-% for i = 1:n_step
-%     % J = h_robot.jacob0(qd.no_opt(:,:,i));
-%     % q0 = null_opt(h_robot, 'manip', q.no_opt(i,:));
-%     q0 = [0 0 2 2 2 2 2 2];
-%     % qns = (eye(8) - pinv(J)*J)*q0';
-%     % qd.no_opt(:,:,i) = pinv(J)*test_v(:,:,i) + qns;
-%     qd.no_opt(:,:,i) = inv(J)*test_v(:,:,i);
-%     J = p560.jacob0(qd.no_opt(:,:,i));
-%     q.no_opt(i+1,:) = q.no_opt(i,:) + qd.no_opt(:,:,i)*dt; 
-% end;
 
 %% Task Objects
 
@@ -104,25 +74,28 @@ manip.qdot = zeros(N-1, 8);
 manip.q0 = zeros(1,8);
 manip.qns = zeros(1,8);
 
-for i = 1: (N)
-    if (i ~= N)
+k0 = 0.03;
+
+for i = 1: (N-1)
         ve(i, :) = tr2delta(TC(:, :, i), TC(:, :, i+1)) / dt;
         
         % No optimization jacobian and joint positions
         no_opt.J = jacob0(robot, no_opt.q(i,:));
-        no_opt.qdot(i,:) = pinv(no_opt.J) * ve(i,:)';
+        no_opt.Jpinv = no_opt.J'*((no_opt.J*no_opt.J')^-1);
+        no_opt.qdot(i,:) = no_opt.Jpinv * ve(i,:)';
         no_opt.q(i+1,:) = no_opt.q(i,:) + (no_opt.qdot(i,:)*dt);
         
         % Null space optimization - Manipulability
         manip.J = jacob0(robot, manip.q(i,:));
-        manip.q0 = [0 0 1 1 1 1 1 1];
-        manip.qns = (eye(8) - pinv(manip.J) * manip.J) * manip.q0';
-        manip.qdot(i,:) = pinv(manip.J) * ve(i,:)' + manip.qns;
-        manip.q(i+1,:) = manip.q(i,:) + (manip.qdot(i,:)*dt);
-    end   
+        manip.Jpinv = manip.J'*((manip.J*manip.J')^-1);
+        manip.q0 = k0*null_opt(robot, 'manip', no_opt.q(i,:), 'no');
+        manip.qns = (eye(8) - manip.Jpinv * manip.J) * manip.q0';
+        % manip.qns = (null(manip.J))*pinv(null(manip.J)) * manip.q0';
+        manip.qdot(i,:) = manip.Jpinv * ve(i,:)' + manip.qns;
+        manip.q(i+1,:) = manip.q(i,:) + (manip.qdot(i,:)*dt);   
 end
 
 %% Plot the robot
 
 robot.plotopt = {'workspace' [-3 3 -3 3 -4 2] 'scale' 0.5, 'jvec'};
-robot.plot(no_opt.q);
+robot.plot(manip.q);
--- null_opt.m
+++ null_opt.m
@@ -1,14 +1,32 @@
-function [ q0 ] = null_opt(r, type, dq0)
-%NULL_OPT Returns the q_0 for null space optimization
+function [ q0 ] = null_opt(r, type, q_k, con)
+%NULL_OPT Returns the q0 for null space optimization
 %   Returns the vector of the joint space velocities obtained using the
 %   optimization specified by 'type'
 
+if strcmp(con, 'yes')
+    lb = r.qlim(:, 1)';
+    ub = r.qlim(:, 2)';
+
+    opt = optimoptions('fmincon', 'Algorithm', 'interior-point', ...
+            'Display', 'off');
+else
+    opt = optimoptions('fminunc', 'Algorithm', 'quasi-newton', ...
+            'Display', 'off');
+end
+    
+
 if type == 'manip'
-    opt = optimoptions('fminunc', 'Algorithm', 'quasi-newton');
-    obj_f = @(x) sqrt(det(jacob0(r,x) * jacob0(r,x)'));
-    q0 = fminunc(obj_f, dq0, opt);
+    % obj_f = @(x) -sqrt(det(jacob0(r,x) * jacob0(r,x)'));
+    obj_f = @(x) -r.maniplty(x, 'yoshikawa');
+
 else
     error('Undefined optimization type!');
+end
+
+if strcmp(con, 'yes')
+    q0 = fmincon(obj_f, q_k, [], [], [], [], lb,  ub, [], opt);
+else
+    q0 = fminunc(obj_f, q_k, opt);
 
 end
 
